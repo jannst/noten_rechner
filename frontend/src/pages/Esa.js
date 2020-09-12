16a92ce383961d09fa8c67339a6658428814d266
Jannik Sturhann <jannik.sturhann@haw-hamburg.de>
2020-09-12 19:22:45 +0200

    add menu

---
+++ frontend/src/pages/Esa.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export const Esa = () => {
+    return <div>
+        <h4>Esa Rechner</h4>
+    </div>;
+};
---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+export const Home = () => {
+    return <div>
+        <h4>Notenrechner</h4>
+        <p>Ein einfacher Rechner zur Bestimmung von ESA Noten</p>
+    </div>;
+};
